302b31079d325e1322a6b07ad733c87d8adc38cf
Marllon Ramos <marllonsteam@gmail.com>
2025-02-12 23:27:08 -0300

    +can`s animations completed: Rotation and position at the second section. Just with 1 canvas

--- slices/Hero/Scene.tsx
+++ slices/Hero/Scene.tsx
@@ -55,11 +55,54 @@ const Scene = () => {
       },
     });
 
-    introTl
-      .from(can1GroupRef.current.position, { y: -5, x: 1 }, 0)
-      .from(can1GroupRef.current.rotation, { z: 3 }, 0)
-      .from(can2GroupRef.current.position, { y: 5, x: 1 }, 0)
-      .from(can2GroupRef.current.rotation, { z: 3 }, 0);
+    if (window.screenY < 20) {
+      introTl
+        .from(can1GroupRef.current.position, { y: -5, x: 1 }, 0)
+        .from(can1GroupRef.current.rotation, { z: 3 }, 0)
+        .from(can2GroupRef.current.position, { y: 5, x: 1 }, 0)
+        .from(can2GroupRef.current.rotation, { z: 3 }, 0);
+    }
+
+    const scrollTl = gsap.timeline({
+      defaults: {
+        duration: 2,
+      },
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTl
+      // Rotate can group
+      .to(groupRef.current.rotation, { y: Math.PI * 2 })
+
+      // Can 1 - black cherry
+      .to(can1Ref.current?.position, { x: -0.2, y: -0.7, z: -2 }, 0)
+      .to(can1Ref.current?.rotation, { z: 0.3 }, 0)
+
+      // Can 2 - Lemon Lime
+      .to(can2Ref.current?.position, { x: 1, y: -0.2, z: -1 }, 0)
+      .to(can2Ref.current?.rotation, { z: 0 }, 0)
+
+      // Can 3 - Grape
+      .to(can3Ref.current?.position, { x: -0.3, y: 0.5, z: -1 }, 0)
+      .to(can3Ref.current?.rotation, { z: -0.1 }, 0)
+
+      // Can 4 - Strawberry Lemonade
+      .to(can4Ref.current?.position, { x: 0, y: -0.3, z: 0.5 }, 0)
+      .to(can4Ref.current?.rotation, { z: 0.3 }, 0)
+
+      // Can 5 - Watermelon
+      .to(can5Ref.current?.position, { x: 0.3, y: 0.5, z: -0.5 }, 0)
+      .to(can5Ref.current?.rotation, { z: -0.25 }, 0)
+      .to(
+        groupRef.current.position,
+        { x: 1, duration: 3, ease: "sine.inOut" },
+        1.3
+      );
   });
 
   return (
